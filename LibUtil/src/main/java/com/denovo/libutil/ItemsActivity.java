e7850e599371c9d69778ed508c53f3180534a985
Sridhar <androidsolver@gmail.com>
2022-03-24 17:25:06 +0530

    Bring UI part in library

---
+++ LibUtil/src/main/java/com/denovo/libutil/ItemsActivity.java
@@ -0,0 +1,15 @@
+package com.denovo.libutil;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
